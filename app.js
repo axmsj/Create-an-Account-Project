d07640e4726b58fcf736c5f5753cd054af06a546
Armando G <115781553+axmsj@users.noreply.github.com>
2023-10-04 06:51:57 -0400

    js variables final

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-firstName = document.querySelector('fname');
-lastName = document.querySelector('lname');
-
-if (firstName === '') {
-  alert('You need to enter your name!');
-}
+const RegistrationForm = document.getElementById('registrationForm');
+const fNameInput = document.getElementById('fname');
+const lNameInput = document.getElementById('lname');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const submitButton = document.getElementById('submitButton');
